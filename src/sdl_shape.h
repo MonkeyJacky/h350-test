ec361411439974eadcf84e440d4a7210c96df46a
jacky <wboy181818@21cn.com>
2013-03-29 17:44:58 +0800

    Deleted useless codes.

--- src/sdl_shape.h
+++ src/sdl_shape.h
@@ -21,22 +21,8 @@ void test_words_show(const char* temp_str, SDL_Color color);
 void sdl_draw_a_pic(SDL_Surface* img, SDL_Rect *src_rect, SDL_Rect *dst_rect);
 void sdl_flip_screen(void);
 void sdl_set_pen(SDL_Color *color, int w);
-
 int sdl_get_pen_width();
-
 void sdl_draw_rect(SDL_Rect *rect);
-
 void sdl_draw_rect_solid(SDL_Rect *rect);
 
-void sdl_draw_rect_dashed(SDL_Surface *screen, SDL_Rect *rect, int gap);
-
-void sdl_draw_circle(SDL_Surface *screen, int xc, int yc, int radius);
-void sdl_draw_circle_solid(SDL_Surface *screen, int xc, int yc, int radius);
-
-void sdl_draw_rect_alpha(SDL_Surface *screen, SDL_Rect *rect, SDL_Color *color, int alpha);
-
-SDL_Surface *sdl_create_rect_alpha(int w,int h,SDL_Color *color, int alpha);
-
-SDL_Surface *sdl_create_surface(int w, int h);
-SDL_Surface * sdl_create_surface_with_color(int w, int h,SDL_Color *color);
 #endif
